6fb123eb4e682e2e8fe397b6ca3fe69398736d1f
sanwenyukaochi <sanwenyukaochi@outlook.com>
2026-03-19 14:17:17 +0800

    optimize: 优化代码

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 export function Logo() {
   return (
     <Link className="site-logo" to="/">
-      <img src="/assets/logo.webp" alt="sanwenyukaochi logo" width="48" height="48" />
+      <img src="/assets/logo.webp" alt="sanwenyukaochi logo" width={"48"} height={"48"} />
       <span>sanwenyukaochi</span>
     </Link>
   )
